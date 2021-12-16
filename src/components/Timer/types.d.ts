00674fc4c2b47d579b8d5debceb6c872af2b5b4c
Kayron <kayron.scabral@gmail.com>
2021-12-16 12:00:27 -0300

    🚨 Fix eslint

--- src/components/Timer/types.d.ts
+++ src/components/Timer/types.d.ts
@@ -1,6 +1,9 @@
-import { AlertProps as AlertMaterialProps } from '@mui/material/Alert';
-import { Props } from '../../types';
+import { MouseEventHandler } from 'react'
+
+import { AlertProps as AlertMaterialProps } from '@mui/material/Alert'
+
+import { Props } from 'types'
 
 export interface AlertProps extends AlertMaterialProps, Props {
-  onClose?: React.MouseEventHandler<HTMLButtonElement>;
+  onClose?: MouseEventHandler<HTMLButtonElement>
 }
